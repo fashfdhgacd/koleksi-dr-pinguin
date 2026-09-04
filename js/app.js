4b52bcee314d04e39fd31fe4d36bf48afc2d8737
fashfdhgacd <readmi559@gmail.com>
2026-09-04 23:14:38 +0700

    load putarin-nav setelah gallery

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 (function () {
   var s = document.createElement('script');
   s.src = 'https://cdn.jsdelivr.net/gh/fashfdhgacd/koleksi-dr-pinguin@64ee4c14c60cca54f5f1cdd6f7b6892c43a2b0a1/js/app.js';
+  s.onload = function () {
+    var n = document.createElement('script');
+    n.src = '/js/putarin-nav.js?t=' + Date.now();
+    document.body.appendChild(n);
+  };
   document.head.appendChild(s);
 })();
