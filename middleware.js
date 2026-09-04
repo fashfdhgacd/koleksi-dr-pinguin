8824dc8300a7a1f0695a7734c781a9a8e02e4423
fashfdhgacd <readmi559@gmail.com>
2026-09-04 18:35:00 +0700

    fix: jangan redirect /?v= ke /v/ yang lagi 500

--- middleware.js
+++ middleware.js
@@ -1,11 +1,3 @@
 export default function middleware(request) {
-  const url = new URL(request.url);
-  if (url.pathname === "/" || url.pathname === "/index.html") {
-    const v = url.searchParams.get("v");
-    if (v) {
-      url.pathname = "/v/" + encodeURIComponent(v);
-      url.search = "";
-      return Response.redirect(url, 301);
-    }
-  }
+  return;
 }
