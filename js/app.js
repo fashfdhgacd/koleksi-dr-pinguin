428323caeec5874aec619ecabe88650c3f47e8ea
fashfdhgacd <readmi559@gmail.com>
2026-09-04 18:42:11 +0700

    restore: load gallery app from last good commit

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-SEE_LOCAL_FILE
+(function () {
+  var s = document.createElement('script');
+  s.src = 'https://cdn.jsdelivr.net/gh/fashfdhgacd/koleksi-dr-pinguin@64ee4c14c60cca54f5f1cdd6f7b6892c43a2b0a1/js/app.js';
+  s.onload = function () {
+    try {
+      var el = document.querySelector('#videoCount');
+      if (!el) return;
+    } catch (e) {}
+  };
+  document.head.appendChild(s);
+})();
