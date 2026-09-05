310c83dad30f955d1e161608f2ce402caa29466f
fashfdhgacd <readmi559@gmail.com>
2026-09-06 02:10:05 +0700

    feat: kartu IndoAV/UserBokep buka halaman /v/

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 (function () {
   var s = document.createElement('script');
-  s.src = 'https://cdn.jsdelivr.net/gh/fashfdhgacd/koleksi-dr-pinguin@64ee4c14c60cca54f5f1cdd6f7b6892c43a2b0a1/js/app.js';
+  s.src = '/js/app-gallery.js?v=watchpage1';
   s.onload = function () {
     var n = document.createElement('script');
     n.src = '/js/putarin-nav.js?t=' + Date.now();
