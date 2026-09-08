8ad0d6c327114b62eded307f015d993c4f06382f
fashfdhgacd <readmi559@gmail.com>
2026-09-09 05:29:19 +0700

    Fallback gallery ke jsDelivr kalau gallery-full belum ada

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
   s.src = '/js/gallery-full.js?v=20260909thumb';
   s.onerror = function () {
     var f = document.createElement('script');
-    f.src = '/js/gallery.js?v=20260907b';
+    f.src = 'https://cdn.jsdelivr.net/gh/fashfdhgacd/koleksi-dr-pinguin@64ee4c14c60cca54f5f1cdd6f7b6892c43a2b0a1/js/app.js';
     f.onload = bootNav;
     document.head.appendChild(f);
   };
