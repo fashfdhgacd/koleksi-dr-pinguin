6c04795db152dd19806c3d81eb16adb9dee6e4f1
fashfdhgacd <readmi559@gmail.com>
2026-09-09 06:46:29 +0700

    Satukan player kit, Videy mov/mp4, fetch data paralel

--- js/app.js
+++ js/app.js
@@ -22,9 +22,9 @@
         if (main) { main.classList.remove('opacity-0'); main.classList.add('opacity-100'); }
       }
     } catch (e) {}
-    ['/js/putarin-nav.js', '/js/modal-share.js', '/js/thumb-fix.js', '/js/modal-close.js', '/js/videy-fix.js'].forEach(function (src) {
+    ['/js/putarin-nav.js', '/js/modal-share.js', '/js/thumb-fix.js', '/js/player-kit.js'].forEach(function (src) {
       var el = document.createElement('script');
-      el.src = src + '?t=' + Date.now();
+      el.src = src + '?v=opt1';
       document.body.appendChild(el);
     });
   }
