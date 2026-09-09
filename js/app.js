22c3a6d7ca07b3d78beb02e5cebc014ee62c1491
fashfdhgacd <readmi559@gmail.com>
2026-09-09 12:51:53 +0700

    Watch page JavHub-style + klik nonton ke /v/

--- js/app.js
+++ js/app.js
@@ -22,9 +22,9 @@
         if (main) { main.classList.remove('opacity-0'); main.classList.add('opacity-100'); }
       }
     } catch (e) {}
-    ['/js/putarin-nav.js', '/js/modal-share.js', '/js/thumb-fix.js', '/js/modal-close.js', '/js/videy-fix.js'].forEach(function (src) {
+    ['/js/open-watch.js', '/js/putarin-nav.js', '/js/modal-share.js', '/js/thumb-fix.js', '/js/modal-close.js', '/js/videy-fix.js'].forEach(function (src) {
       var el = document.createElement('script');
-      el.src = src + '?v=back3';
+      el.src = src + '?v=watchpage1';
       document.body.appendChild(el);
     });
   }
