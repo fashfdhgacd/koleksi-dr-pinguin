a31722bcb65d88e3664516036ce6ebfcbb8f832e
fashfdhgacd <readmi559@gmail.com>
2026-09-09 19:11:29 +0700

    Load modal-share langsung dari index biar tidak tergantung gallery-full

--- js/app.js
+++ js/app.js
@@ -24,7 +24,7 @@
     } catch (e) {}
     ['/js/putarin-nav.js', '/js/modal-share.js', '/js/thumb-fix.js', '/js/modal-close.js', '/js/videy-fix.js'].forEach(function (src) {
       var el = document.createElement('script');
-      el.src = src + '?v=avthumb2';
+      el.src = src + '?v=hubfix1';
       document.body.appendChild(el);
     });
   }
