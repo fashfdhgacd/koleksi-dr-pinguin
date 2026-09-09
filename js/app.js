0fd0866d055170b82ec1c3cd44108bd517d8c25a
fashfdhgacd <readmi559@gmail.com>
2026-09-09 18:48:56 +0700

    Ambil thumbnail IndoAV/userbokep dari poster embed

--- js/app.js
+++ js/app.js
@@ -24,7 +24,7 @@
     } catch (e) {}
     ['/js/putarin-nav.js', '/js/modal-share.js', '/js/thumb-fix.js', '/js/modal-close.js', '/js/videy-fix.js'].forEach(function (src) {
       var el = document.createElement('script');
-      el.src = src + '?v=hubmodal3';
+      el.src = src + '?v=avposter1';
       document.body.appendChild(el);
     });
   }
