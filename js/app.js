701334d3bbb5afcdf2a0126fdaa87af307f6ecf7
fashfdhgacd <readmi559@gmail.com>
2026-09-09 07:06:01 +0700

    Balikkan: HP tidak redirect ke /v/

--- js/app.js
+++ js/app.js
@@ -22,13 +22,9 @@
         if (main) { main.classList.remove('opacity-0'); main.classList.add('opacity-100'); }
       }
     } catch (e) {}
-    var scripts = ['/js/putarin-nav.js', '/js/thumb-fix.js', '/js/open-watch.js'];
-    if (window.innerWidth > 1024) {
-      scripts.push('/js/modal-share.js', '/js/modal-close.js', '/js/videy-fix.js');
-    }
-    scripts.forEach(function (src) {
+    ['/js/putarin-nav.js', '/js/modal-share.js', '/js/thumb-fix.js', '/js/modal-close.js', '/js/videy-fix.js'].forEach(function (src) {
       var el = document.createElement('script');
-      el.src = src + '?v=hpB1';
+      el.src = src + '?v=back3';
       document.body.appendChild(el);
     });
   }
