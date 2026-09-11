7e5fab63cb61dd074d4d9e3d2cb9557d1a376152
fashfdhgacd <readmi559@gmail.com>
2026-09-12 00:10:58 +0700

    fix: cache-bust loader; hero poster bukan embed

--- js/app.js
+++ js/app.js
@@ -6,14 +6,14 @@
   }
   var css = document.createElement("link");
   css.rel = "stylesheet";
-  css.href = "/css/home-lite.css?v=1";
+  css.href = "/css/home-lite.css?v=2";
   document.head.appendChild(css);
   add("/js/kdp-bus.js?v=2");
-  add("/js/kdp-overlay.js?v=5");
-  add("/js/kdp-persist.js?v=1");
+  add("/js/kdp-overlay.js?v=6");
+  add("/js/kdp-persist.js?v=2");
   add("/js/nav-fix.js?v=2");
   add("/js/share-sheet.js?v=1");
-  add("/js/home-gallery.js?v=1");
+  add("/js/home-gallery.js?v=2");
   setTimeout(function () {
     add("/js/modal-share.js?v=rec10");
   }, 400);
