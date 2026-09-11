b65133db3e9c14815b838f03dc9fe6814dc5ff03
fashfdhgacd <readmi559@gmail.com>
2026-09-11 20:32:12 +0700

    Bust cache nav-fix

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@
     document.body.appendChild(s);
   }
   add("/js/kdp-overlay.js?v=3");
-  add("/js/nav-fix.js?v=1");
+  add("/js/nav-fix.js?v=2");
   add("/js/share-sheet.js?v=1");
   var css = document.createElement("link");
   css.rel = "stylesheet";
